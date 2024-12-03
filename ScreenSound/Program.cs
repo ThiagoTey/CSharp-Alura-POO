15aba6017d17b3bb382d49f1d725f1517e50426d
Thiago <teyextreme@outlook.com>
2024-12-03 12:13:49 -0300

    Adicionando funções em classes

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,9 +1,15 @@
 ﻿Musica musica1 = new Musica();
 musica1.nome = "Dead memories";
 musica1.artista = "Slipknot";
+musica1.duracao = 300;
+musica1.disponivel = true;
 
-Console.WriteLine($"Nome da musica {musica1.nome}");
+musica1.ExibirFichaTecnica();
 
 Musica musica2 = new Musica();
 musica2.nome = "In The End";
 musica2.artista = "Linkin Park";
+musica2.duracao = 323;
+musica2.disponivel = false;
+
+musica2.ExibirFichaTecnica();
