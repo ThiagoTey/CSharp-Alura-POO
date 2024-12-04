b6b906e55ce3ed90b106b0514d898bb816b4704f
Thiago <teyextreme@outlook.com>
2024-12-04 20:34:28 -0300

    Moficando classe Musica~

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -2,7 +2,8 @@
 musica1.nome = "Dead memories";
 musica1.artista = "Slipknot";
 musica1.duracao = 300;
-musica1.disponivel = true;
+musica1.EscreveDisponivel(true);
+Console.WriteLine(musica1.LeDisponivel());
 
 musica1.ExibirFichaTecnica();
 
@@ -10,6 +11,6 @@ Musica musica2 = new Musica();
 musica2.nome = "In The End";
 musica2.artista = "Linkin Park";
 musica2.duracao = 323;
-musica2.disponivel = false;
+musica2.EscreveDisponivel(false);
 
 musica2.ExibirFichaTecnica();
