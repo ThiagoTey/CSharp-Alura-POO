3249c6c99eac257b7c03e788aa09e097235680bc
Thiago <teyextreme@outlook.com>
2024-12-05 21:10:19 -0300

    Aprendento proprieties da classe

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -2,8 +2,8 @@
 musica1.nome = "Dead memories";
 musica1.artista = "Slipknot";
 musica1.duracao = 300;
-musica1.EscreveDisponivel(true);
-Console.WriteLine(musica1.LeDisponivel());
+musica1.Disponivel = true;
+Console.WriteLine(musica1.Disponivel);
 
 musica1.ExibirFichaTecnica();
 
@@ -11,6 +11,6 @@ Musica musica2 = new Musica();
 musica2.nome = "In The End";
 musica2.artista = "Linkin Park";
 musica2.duracao = 323;
-musica2.EscreveDisponivel(false);
+musica2.Disponivel = false;
 
 musica2.ExibirFichaTecnica();
