7027b80b836ea83bed96e515064d5755f7907faf
Thiago <teyextreme@outlook.com>
2024-12-06 15:50:53 -0300

    lambda

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,16 +1,16 @@
 ﻿Musica musica1 = new Musica();
-musica1.nome = "Dead memories";
-musica1.artista = "Slipknot";
-musica1.duracao = 300;
+musica1.Nome = "Dead memories";
+musica1.Artista = "Slipknot";
+musica1.Duracao = 300;
 musica1.Disponivel = true;
 Console.WriteLine(musica1.Disponivel);
 
 musica1.ExibirFichaTecnica();
 
 Musica musica2 = new Musica();
-musica2.nome = "In The End";
-musica2.artista = "Linkin Park";
-musica2.duracao = 323;
+musica2.Nome = "In The End";
+musica2.Artista = "Linkin Park";
+musica2.Duracao = 323;
 musica2.Disponivel = false;
 
 musica2.ExibirFichaTecnica();
