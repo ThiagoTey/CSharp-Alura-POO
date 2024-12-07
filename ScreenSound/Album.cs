3590cd04bc3032de13e72eee6302f9aef836c272
Thiago <teyextreme@outlook.com>
2024-12-07 10:28:36 -0300

    Atuaizar classe album

--- ScreenSound/Album.cs
+++ ScreenSound/Album.cs
@@ -2,10 +2,20 @@
 {
     private List<Musica> musicas = new List<Musica>();
     public string Nome { get; set; }
-    public int DuracaoTotal { get; set; }
+    public int DuracaoTotal => musicas.Sum(m => m.Duracao);
 
     public void AdicionarMusica(Musica musica)
     {
         musicas.Add(musica);
+    }
+
+    public void ExibirMusicasDoAlbum()
+    {
+        Console.WriteLine($"Lista de músicas do álbul {Nome}");
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine($"Música : {musica.Nome}");
+        }
+        Console.WriteLine($"Tempo todas do álbum : {DuracaoTotal}");
     }
 }
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -2,10 +2,14 @@
 albumQueen.Nome = "A night at the opera";
 
 Musica musica1 = new Musica();
-musica1.Nome = "Love of my live";
+musica1.Nome = "Love of my life";
+musica1.Duracao = 213;
 
 Musica musica2 = new Musica();
 musica2.Nome = "Bohemian Rhapsody";
+musica1.Duracao = 354;
 
 albumQueen.AdicionarMusica(musica1);
 albumQueen.AdicionarMusica(musica2);
+
+albumQueen.ExibirMusicasDoAlbum();
