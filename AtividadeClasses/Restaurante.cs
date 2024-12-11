edc81e649f3e3001be22f8e4b400057c1d8dd6e9
Thiago <teyextreme@outlook.com>
2024-12-11 14:51:07 -0300

    Classe de Restaurante

--- AtividadeClasses/Restaurante.cs
+++ AtividadeClasses/Restaurante.cs
@@ -1,19 +1,32 @@
-﻿class Restaurante
-{
+﻿
+using System.Security.Cryptography.X509Certificates;
 
+class ProdutoRestaurante
+{
+    public string Nome { get; set; }
+    public double Preco { get; set; }
+}
+class Restaurante
+{
+    public List<Mesa> Mesas { get; set; }
+    public Cardapio Cardapio { get; set; }
 }
 
 class Mesa
 {
-
+    public int NumeroMesa { get; set; }
+    public bool Resenvada { get; set; }
+    public List<Pedido> Pedidos { get; set; }
 }
 
 class Pedido
 {
+    public Produto Produto { get; set; }
+    public int Quantidade { get; set; }
 
 }
 
 class Cardapio
 {
-
+    public List<ProdutoRestaurante> Produtos { get; set; }
 }
