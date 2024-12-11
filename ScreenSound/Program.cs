534cd6d2ce6187b8710a6e42aebfb61e05f623a5
Thiago <teyextreme@outlook.com>
2024-12-11 17:26:21 -0300

    Construtor Na classe Musica

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,11 +1,14 @@
-﻿Album albumQueen = new Album();
+﻿Banda queen = new Banda();
+queen.Nome = "Queen";
+
+Album albumQueen = new Album();
 albumQueen.Nome = "A night at the opera";
 
-Musica musica1 = new Musica();
+Musica musica1 = new Musica(queen);
 musica1.Nome = "Love of my life";
 musica1.Duracao = 213;
 
-Musica musica2 = new Musica();
+Musica musica2 = new Musica(queen);
 musica2.Nome = "Bohemian Rhapsody";
 musica1.Duracao = 354;
 
@@ -14,7 +17,5 @@ albumQueen.AdicionarMusica(musica2);
 
 albumQueen.ExibirMusicasDoAlbum();
 
-Banda queen = new Banda();
-queen.Nome = "Queen";
 queen.AdicionarAlbum(albumQueen);
 queen.Exibrdiscografia();
