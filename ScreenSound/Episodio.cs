b429ec6ff012d58981735d675220084ee202239c
Thiago <teyextreme@outlook.com>
2024-12-14 09:54:28 -0300

    Desafio ScreenSound

---
+++ ScreenSound/Episodio.cs
@@ -0,0 +1,20 @@
+﻿class Episodio(int ordem, int duracao, string titulo)
+{
+    private string convidados = "";
+    public int Ordem { get; } = ordem;
+    public int Duracao { get; } = duracao;
+    public string Titulo { get; } = titulo;
+    public string Resumo
+    {
+        get => $"Episódio {Ordem}, Duração: {Duracao} minutos, Convidados: {convidados}";
+    }
+
+    public void AdicionarConvidados(string convidado)
+    {
+        if (!string.IsNullOrEmpty(convidados))
+        { 
+            convidados += ", ";
+        }
+        convidados += convidado;
+    }
+}
---
+++ ScreenSound/Podcast.cs
@@ -0,0 +1,21 @@
+﻿class Podcast(string nome, string host)
+{
+    public string Nome { get; } = nome;
+    public string Host { get; } = host;
+    public List<Episodio> Episodios { get; private set; }
+
+    public void AdicionarEpisodio(Episodio episodio)
+    {
+        Episodios.Add(episodio);
+    }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Nome Podcast: {Nome}, Host: {Host}");
+        Console.WriteLine("Episódios :");
+        foreach (Episodio episodio in Episodios)
+        {
+            Console.WriteLine(episodio.Resumo);
+        }
+    }
+}
